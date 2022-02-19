db8b0a735f02bb29c2667be7d4a9df2236109cd2
Avendaosander <avendano.ramirez@gmail.com>
2022-02-19 19:26:13 -0400

    4to Commit

--- controllers/lista.js
+++ controllers/lista.js
@@ -1,5 +1,39 @@
 class listado{
-   constructor(req, res){
-      this.articulo = [ req.body.articulo ]
+   constructor(){
+      this.articulos = [];
+   }
+
+   agregarArt(data){
+      this.articulos.push(data);
+   }
+
+   mostrarList(res){
+      let ListaArt=[];
+      this.articulos.forEach(e => {
+         ListaArt.push({
+            titulo: e.titulo
+         })
+      });
+      res.status(200).json(ListaArt);
+   }
+   buscarArt(data,res){
+      let artEncontrado = this.articulos.find(e=> e.titulo == data);
+      if(artEncontrado==undefined){
+         res.status(400).send('el articulo que desea ver no existe');
+      }else{
+         res.status(200).json(artEncontrado);
+      }
+   }
+
+   editarArt(art,data,res){
+      let artEncontrado = this.articulos.find(e => e.titulo == art);
+      if(artEncontrado==undefined){
+         res.status(400).send('El articulo que desea editar no existe');
+      }else{
+         this.articulos[this.articulos.indexOf(artEncontrado)] = data;
+         res.status(200).send('Articulo Modificado');
+      }
    }
 }
+
+module.exports = listado;
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,69 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs')
 const { validarCampos, validarCamposVideo } = require('../validators/validator')
 const articulo = require('../controllers/articulo')
+const listado = require('../controllers/lista');
+// const archJson = require('../articulos.json')
 
-var nuevoArticulo;
+const jsonArticulos = fs.readFileSync('articulos.json', 'utf-8')
+let articulos = JSON.parse(jsonArticulos)
+// console.log(articulos[1])
+
+let nuevoArticulo;
+let Listado = new listado();
+
+articulos.forEach(element => {
+  Listado.agregarArt(element)
+});
+// console.log(Listado.articulos)
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.json({ title: 'Api Rest' });
 });
 
+// GET para mostrar articulos
+router.get('/articulos', (req,res)=>{
+  if(Listado === undefined){
+    res.status(400).send('No hay articulos');
+  }else{
+    Listado.mostrarList(res);
+  }
+})
+
+//GET para mostrar por nombre
+router.get('/articulo/:titulo', (req,res)=>{
+  if(Listado === undefined){
+    res.send('No hay articulos')
+  }else{
+    Listado.buscarArt(req.params.titulo,res)
+  }
+})
+
 router.post('/new-article', validarCampos, (req, res)=>{
   nuevoArticulo = new articulo(req, res)
-  console.log(nuevoArticulo)
+  Listado.agregarArt(nuevoArticulo);
+  console.log(Listado.articulos)
+  // articulos.push()
+  // let jsonArticulos = JSON.stringify(articulos)
+  // fs.writeFileSync('articulos.json', jsonArticulos, 'utf-8')
   res.status(200).send("Articulo Creado Correctamente")
 })
 
 router.post('/new-article-video', validarCamposVideo, (req, res)=>{
   nuevoArticulo = new articulo(req, res)
+  Listado.agregarArt(nuevoArticulo);
   console.log(nuevoArticulo)
   res.status(200).send("Articulo Creado Correctamente")
+})
+
+router.put('/modificarArt/:titulo', (req,res)=>{
+  if(Listado === undefined){
+    res.status(400).send('No hay articulos disponibles')
+  }else{
+    Listado.editarArt(req.params.titulo, req.body, res);
+  }
 })
 
 module.exports = router;
